43de937d18f566e5fa4b8e9d9ab5dae703d4867e
Anil Dhakal <aneildhakal21@gmail.com>
2025-05-18 17:17:19 -0400

    changed main

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { ThemeProvider } from "./components/theme-provider";
 import { AsyncAlertContextProvider } from "./hooks/async-alert-dialog";
 import "./index.css";
 
@@ -13,17 +12,17 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <GoogleOAuthProvider clientId="292000457428-0r959um9ivqddgulv8ds9llrhp1cf259.apps.googleusercontent.com">
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <AsyncAlertContextProvider>
-              <App />
-            </AsyncAlertContextProvider>
-          </BrowserRouter>
-          <Toaster />
-        </QueryClientProvider>
-      </GoogleOAuthProvider>
-    </ThemeProvider>
+    {/* <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme"> */}
+    <GoogleOAuthProvider clientId="292000457428-0r959um9ivqddgulv8ds9llrhp1cf259.apps.googleusercontent.com">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AsyncAlertContextProvider>
+            <App />
+          </AsyncAlertContextProvider>
+        </BrowserRouter>
+        <Toaster />
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
+    {/* </ThemeProvider> */}
   </StrictMode>
 );
