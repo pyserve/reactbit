939ac1f36eec306c1d6d5f484f1dc9db94aa583c
Anil Dhakal <aneildhakal21@gmail.com>
2025-05-20 08:15:42 -0400

    updated protocol

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,6 +1,6 @@
 export const httpProtocol = window.location.protocol;
 console.log("🚀 ~ httpProtocol:", httpProtocol);
-export const protocol = httpProtocol === "https" ? "wss" : "ws";
+export const protocol = httpProtocol === "https:" ? "wss" : "ws";
 console.log("🚀 ~ protocol:", protocol);
 export const backend_host = "reactbit-backend.onrender.com"; // "reactbit-backend.onrender.com"
 export const backend_url = `${httpProtocol}//reactbit-backend.onrender.com`; // "reactbit-backend.onrender.com"
